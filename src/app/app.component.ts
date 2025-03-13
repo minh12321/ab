f257fe2f6a5a5b2b93c1fdcbd668c2a43223071c
minh12321 <caominhh725@gmail.com>
2025-03-13 11:09:45 +0700

    home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,42 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, Router } from '@angular/router';
+import { NgClass,NgFor,NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NgClass,NgFor,NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'ab';
+
+  constructor(private router: Router) {}
+
+  goToreg() {
+    this.router.navigate(['/reg']);
+  }
+
+  goTolog() {
+    this.router.navigate(['/log']);
+  }
+
+  menus = [
+    { title: 'Trang Chủ', icon: 'fa-solid fa-house',route:'/home', active: false,submenus: ['Overview', 'Stats']},
+    { title: 'Giày hiệu', icon: 'fa-solid fa-shoe-prints',route:'/home', active: false ,submenus: ['Overview', 'Stats']},
+    { title: 'Giày đinh', icon: 'fa-solid fa-shoe-prints',route:'/home', active: false,submenus: ['Overview', 'Stats'] },
+    { title: 'Găng tay', icon: 'fa-solid fa-hand',route:'/home', active: false,submenus: ['Overview', 'Stats'] },
+    { title: 'Phụ kiện', icon: 'fa-solid fa-toolbox',route:'/home', active: false,submenus: ['Overview', 'Stats'] },
+    { title: 'Danh mục', icon: 'fa-solid fa-list',route:'/home', active: false,submenus: ['Overview', 'Stats'] }
+  ];
+
+  toggleMenu(selectedMenu: any) {
+    console.log("Menu clicked:", selectedMenu);
+    this.menus.forEach(menu => {
+      if (menu !== selectedMenu) {
+        menu.active = false;
+      }
+    });
+    selectedMenu.active = !selectedMenu.active;
+  }
 }
---
+++ src/app/reg/reg.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reg',
+  imports: [],
+  templateUrl: './reg.component.html',
+  styleUrl: './reg.component.css'
+})
+export class RegComponent {
+
+}
