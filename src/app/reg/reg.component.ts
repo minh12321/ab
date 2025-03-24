ccbe7e5965dd27861d4d651ca8676ba0b5c3f1a0
minh12321 <caominhh725@gmail.com>
2025-03-25 00:52:45 +0700

    beta logreg

--- src/app/reg/reg.component.ts
+++ src/app/reg/reg.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { UserService } from '../../api-sevice/user.service';
+import { Router } from '@angular/router';
+import { HttpParams } from '@angular/common/http';
 
 @Component({
   selector: 'app-reg',
@@ -7,5 +10,29 @@ import { Component } from '@angular/core';
   standalone:false
 })
 export class RegComponent {
+  username: string = '';
+  password: string = '';
+  fullName: string = '';
+  email :string='';
 
+  constructor(private userService: UserService, private router: Router) { }
+
+  register() {
+    const params = new HttpParams()
+          .set('username', this.username)
+          .set('password', this.password)
+          .set('fullName',this.fullName)
+          .set('email',this.email);
+    this.userService.registerUser(params).subscribe({
+      next: () => {
+        alert('register complete');
+        this.router.navigate(['/']);
+      },
+      error: (err) => {
+        alert('register failed: ' + err.message);
+      }
+    })      
+
+  }
+  
 }
