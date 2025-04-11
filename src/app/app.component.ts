6be9274cd845954697012577d77cf05d8ea6dca6
minh12321 <caominhh725@gmail.com>
2025-04-12 05:32:37 +0700

    mini menu-

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, Inject, NgZone, PLATFORM_ID } from '@angular/core';
 import { RouterOutlet, Router } from '@angular/router';
 import { NgClass,NgFor,NgIf } from '@angular/common';
 import { AuthService } from './auth/auth.service';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -14,7 +15,29 @@ export class AppComponent {
   username: string = '';
   loginstatus: boolean = false;
 
-  constructor(private authService: AuthService,private router: Router) {}
+  constructor(private ngZone: NgZone,@Inject(PLATFORM_ID) private platformId: Object,private authService: AuthService,private router: Router) {
+    this.isBrowser = isPlatformBrowser(this.platformId);
+
+    if (this.isBrowser) {
+      const SpeechRecognition = (window as any).SpeechRecognition || (window as any).webkitSpeechRecognition;
+      this.recognition = new SpeechRecognition();
+
+      this.recognition.lang = 'vi-VN';
+      this.recognition.interimResults = false;
+      this.recognition.maxAlternatives = 1;
+
+      this.recognition.onresult = (event: any) => {
+        const transcript = event.results[0][0].transcript;
+        this.ngZone.run(() => {
+          this.text = transcript;
+        });
+      };
+
+      this.recognition.onerror = (event: any) => {
+        console.error('Lỗi nhận giọng nói:', event.error);
+      };
+    }
+  }
 
   ngOnInit(): void { 
     this.ngu();
@@ -37,27 +60,27 @@ export class AppComponent {
     { 
       title: 'Sản Phẩm', icon: 'fa-solid fa-shoe-prints', active: false, 
       submenus: [
-        { title: 'Overview', route: '/giay-hieu/overview' },
-        { title: 'Stats', route: '/giay-hieu/stats' },
-        { title: 'Stats', route: '/giay-hieu/stats' },
-        { title: 'Stats', route: '/giay-hieu/stats' },
-        { title: 'Stats', route: '/giay-hieu/stats' },
-        { title: 'Stats', route: '/giay-hieu/stats' },
-        { title: 'Stats', route: '/giay-hieu/stats' },
-        { title: 'Stats', route: '/giay-hieu/stats' },
-        { title: 'Stats', route: '/giay-hieu/stats' },
-        { title: 'Stats', route: '/giay-hieu/stats' },
-        { title: 'Stats', route: '/giay-hieu/stats' },
-        { title: 'Stats', route: '/giay-hieu/stats' },
-        { title: 'Stats', route: '/giay-hieu/stats' },
-        { title: 'Stats', route: '/giay-hieu/stats' },
-        { title: 'Stats', route: '/giay-hieu/stats' },
-        { title: 'Stats', route: '/giay-hieu/stats' },
-        { title: 'Stats', route: '/giay-hieu/stats' },
-        { title: 'Stats', route: '/giay-hieu/stats' },
-        { title: 'Stats', route: '/giay-hieu/stats' },
-        { title: 'Stats', route: '/giay-hieu/stats' },
-        { title: 'Stats', route: '/giay-hieu/stats' },
+        { title: 'Nike', route: '/san-pham?hang=Nike' },
+        { title: 'Adidas', route: '/san-pham?hang=Adidas' },
+        { title: 'Beck', route: '/san-pham?hang=Beck' },
+        { title: 'Kamito', route: '/san-pham?hang=Kamito' },
+        { title: 'Puma', route: '/san-pham?hang=Puma' },
+        { title: 'Giầy đinh', route: '/san-pham?kieuGiay=Giầy đinh' },
+        { title: 'Giầy lười', route: '/san-pham?kieuGiay=Giầy lười' },
+        { title: 'Giầy cổ cao', route: '/san-pham?kieuGiay=Giầy cổ cao' },
+        { title: 'Gang tay', route: '/san-pham?loaihang=Gang tay' },
+        { title: 'Bóng', route: '/san-pham?loaihang=Bóng' },
+        { title: 'Balo', route: '/san-pham?loaihang=Balo' },
+        { title: 'Quần', route: '/san-pham?loaihang=Quần' },
+        { title: 'Áo', route: '/san-pham?loaihang=Áo' },
+        { title: 'Tất', route: '/san-pham?loaihang=Tất' },
+        { title: 'Băng Keo', route: '/san-pham?loaihang=Băng Keo' },
+        { title: 'Bình xịt', route: '/san-pham?loaihang=Bình xịt' },
+        { title: 'Dây giày', route: '/san-pham?loaihang=Dây giày' },
+        { title: 'khác', route: '/giay-hieu/stats' },
+        { title: 'khác', route: '/giay-hieu/stats' },
+        { title: 'khác', route: '/giay-hieu/stats' },
+        { title: 'khác', route: '/giay-hieu/stats' },
       ] 
     },
     { title: 'Chính sách', icon: 'fa-solid fa-shoe-prints',},
@@ -76,5 +99,14 @@ export class AppComponent {
 
   navigateTocart() {
     this.router.navigate(['/cart']);
+  }
+
+  // ---------------------------------------------------------------------------------------
+  text = '';
+  recognition: any;
+  isBrowser: boolean;
+
+  startListening() {
+    this.recognition.start();
   }
 }
---
+++ src/app/chinhsach/chinhsach.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chinhsach',
+  imports: [],
+  templateUrl: './chinhsach.component.html',
+  styleUrl: './chinhsach.component.css'
+})
+export class ChinhsachComponent {
+
+}
