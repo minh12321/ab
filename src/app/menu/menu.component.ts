96c0c6e44953a60797c74355f52442c674fd58ce
minh12321 <caominhh725@gmail.com>
2025-04-12 00:14:45 +0700

    logout

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
     selector: 'app-menu',
@@ -9,7 +10,7 @@ import { Router } from '@angular/router';
 })
 export class MenuComponent {
   
-  constructor(private router: Router) { }
+  constructor(private router: Router,private authService :AuthService) { }
     menuOpen = false;
   
     toggleMenu() {
@@ -34,6 +35,9 @@ export class MenuComponent {
     }
     navigateTopo() {
       this.router.navigate(['/prodexe']);
+    }
+    logout(){
+      this.authService.logout();
     }
   
 }
