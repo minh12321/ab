508defd7be88489b3c66f7782742946279ad9eb6
minh12321 <caominhh725@gmail.com>
2025-04-12 02:54:33 +0700

    hm hơi ll

--- src/main.ts
+++ src/main.ts
@@ -9,16 +9,13 @@ platformBrowserDynamic().bootstrapModule(AppModule)
 
   const originalAlert = window.alert;
   window.alert = function(message: string): void {
-  // Gọi alert gốc
-  // originalAlert(message);
 
-  // Gọi alert custom
   const alertBox = document.getElementById('custom-alert');
   if (alertBox) {
     alertBox.textContent = message;
     alertBox.style.display = 'block';
 
-    // Tự động ẩn sau 3 giây
+
     setTimeout(() => {
       alertBox!.style.display = 'none';
     }, 3000);
