6bfc42e2deb0ecaef3a2229e06436362b3204320
minh12321 <caominhh725@gmail.com>
2025-04-12 04:26:16 +0700

    menu_nav_change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,50 +24,56 @@ export class AppComponent {
   ngu(): boolean {
     if (this.authService.isLoggedIn()) {
       return this.loginstatus=true;
-    } else {
-      return this.loginstatus=false;
-    }
+    } else {return this.loginstatus=false;}
   }
-  
-
   goToreg() {
     this.router.navigate(['/reg']);
   }
-
   goTolog() {
     this.router.navigate(['/log']);
   }
-
   menus = [
     { title: 'Trang Chủ', icon: 'fa-solid fa-house', route: '/home' },
     { 
       title: 'Sản Phẩm', icon: 'fa-solid fa-shoe-prints', active: false, 
       submenus: [
         { title: 'Overview', route: '/giay-hieu/overview' },
-        { title: 'Stats', route: '/giay-hieu/stats' }
+        { title: 'Stats', route: '/giay-hieu/stats' },
+        { title: 'Stats', route: '/giay-hieu/stats' },
+        { title: 'Stats', route: '/giay-hieu/stats' },
+        { title: 'Stats', route: '/giay-hieu/stats' },
+        { title: 'Stats', route: '/giay-hieu/stats' },
+        { title: 'Stats', route: '/giay-hieu/stats' },
+        { title: 'Stats', route: '/giay-hieu/stats' },
+        { title: 'Stats', route: '/giay-hieu/stats' },
+        { title: 'Stats', route: '/giay-hieu/stats' },
+        { title: 'Stats', route: '/giay-hieu/stats' },
+        { title: 'Stats', route: '/giay-hieu/stats' },
+        { title: 'Stats', route: '/giay-hieu/stats' },
+        { title: 'Stats', route: '/giay-hieu/stats' },
+        { title: 'Stats', route: '/giay-hieu/stats' },
+        { title: 'Stats', route: '/giay-hieu/stats' },
+        { title: 'Stats', route: '/giay-hieu/stats' },
+        { title: 'Stats', route: '/giay-hieu/stats' },
+        { title: 'Stats', route: '/giay-hieu/stats' },
+        { title: 'Stats', route: '/giay-hieu/stats' },
+        { title: 'Stats', route: '/giay-hieu/stats' },
       ] 
     },
-    { 
-      title: 'Giày đinh', icon: 'fa-solid fa-shoe-prints', active: false, 
-      submenus: [
-        { title: 'Overview', route: '/log' },
-        { title: 'Stats', route: '/giay-dinh/stats' }
-      ] 
-    },
-    { title: 'Găng tay', icon: 'fa-solid fa-hand', route: '/gang-tay' },
-    { title: 'Phụ kiện', icon: 'fa-solid fa-toolbox', route: '/phu-kien' },
-    { title: 'Danh mục', icon: 'fa-solid fa-list', route: '/danh-muc' }
+    { title: 'Chính sách', icon: 'fa-solid fa-shoe-prints',},
+    { title: 'Liên Hệ', icon: 'fa-solid fa-hand', route: '/gang-tay' },
+    { title: '  Hạng  ', icon: 'fa-solid fa-toolbox', route: '/phu-kien' },
+    { title: 'Voucher', icon: 'fa-solid fa-list', route: '/danh-muc' }
   ];      
 
-  toggleMenu(selectedMenu: any) {
-    console.log("Menu clicked:", selectedMenu);
-    this.menus.forEach(menu => {
-      if (menu !== selectedMenu) {
-        menu.active = false;
-      }
-    });
-    selectedMenu.active = !selectedMenu.active;
+  toggleSubmenu(event: Event) {
+    const parentLi = (event.currentTarget as HTMLElement);
+    const submenu = parentLi.querySelector('.submenu');
+    if (submenu) {
+      submenu.classList.toggle('hidden');
+    }
   }
+
   navigateTocart() {
     this.router.navigate(['/cart']);
   }
