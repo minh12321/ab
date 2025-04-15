1465c26fdbc6e773e5958a0881ed1c66d8932a8f
minh12321 <caominhh725@gmail.com>
2025-04-16 03:52:54 +0700

    :vv

--- src/app/page/gio-hang/gio-hang.component.ts
+++ src/app/page/gio-hang/gio-hang.component.ts
@@ -4,6 +4,7 @@ import { AuthService } from '../../auth/auth.service';
 import { ProductService } from '../../../api-sevice/san_pham.service';
 import { CartService } from '../../../api-sevice/cart.service';
 import { CartItem } from '../../../api-sevice/cart-item.model';
+import { HoaDonService } from '../../../api-sevice/hoa_don.service';
 
 
 @Component({
@@ -17,10 +18,38 @@ export class GioHangComponent implements OnInit {
   product: CartItem[] = [];
 
   confirmOrder() {
+    const username = this.authService.getid().toString();
+    const currentDate = new Date().toISOString().slice(0, 10); // yyyy-MM-dd
+  
+    if (!username) {
+      alert('Vui lòng đăng nhập trước khi đặt hàng.');
+      return;
+    }
+    this.product.forEach(item => {
+      const hoaDon = {
+        maHoaDon: 0,
+        maKhachHang: username,
+        tenhang: item.productName,
+        gia: item.totalPrice,
+        soLuong: item.quantity,
+        ngayMua: currentDate,
+        trangthai: 'Chờ xác nhận'
+      };
+  
+      this.hoaDonService.createHoaDon(hoaDon).subscribe({
+        next: () => {
+          console.log('Hóa đơn đã được tạo:', hoaDon);
+        },
+        error: (err) => {
+          console.error('Lỗi khi tạo hóa đơn:', err);
+        }
+      });
+    });
+  
     alert('Đơn hàng đã được chốt!');
-
   }
   constructor(
+    private hoaDonService: HoaDonService,
       private route: ActivatedRoute,
       private cartService: CartService,
       private productService: ProductService,
