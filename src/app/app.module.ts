771105e80df181eaef881e0016de0845df4c3349
longlop <tuanlongp70@gmail.com>
2025-04-25 00:54:22 +0700

    tạm2tạm2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
-import { NgModule } from '@angular/core';
-import { NgChartsModule } from 'ng2-charts';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { provideHttpClient, withFetch,  withInterceptorsFromDi } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { LoadingSpinnerComponent } from './components/loading-spinner/loading-spinner.component';
-import { AppRoutingModule } from './app-routing.module';
+import { provideHttpClient, withFetch, withInterceptorsFromDi } from '@angular/common/http';
+import { NgChartsModule } from 'ng2-charts';
+
+// COMPONENT
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { LoadingSpinnerComponent } from './components/loading-spinner/loading-spinner.component';
 import { RegComponent } from './page/reg/reg.component';
 import { LoginComponent } from './page/login/login.component';
 import { HomeComponent } from './page/home/home.component';
@@ -20,30 +22,42 @@ import { ChinhsachComponent } from './page/chinhsach/chinhsach.component';
 import { RankComponent } from './page/rank/rank.component';
 import { ThongtinsanphamComponent } from './page/thongtinsanpham/thongtinsanpham.component';
 import { GioHangComponent } from './page/gio-hang/gio-hang.component';
+
+// MODULE CHO SLIDER
 import { SlickCarouselModule } from 'ngx-slick-carousel';
 
-@NgModule({ declarations: [
-        AppComponent,
-        LoadingSpinnerComponent,
-        RegComponent,
-        LoginComponent,
-        HomeComponent,
-        AccountComponent,
-        AdminComponent,
-        MenuComponent,
-        ThemSanPhamComponent,
-        TabNavigationComponent,
-        SanphamComponent,
-        LienheComponent,
-        ChinhsachComponent,
-        RankComponent,
-        ThongtinsanphamComponent,
-        GioHangComponent
-    ],
-    bootstrap: [AppComponent], imports: [BrowserModule,
-        AppRoutingModule,
-        NgChartsModule,
-        FormsModule,
-        SlickCarouselModule,
-        ReactiveFormsModule], providers: [provideHttpClient(withFetch()), provideHttpClient(withInterceptorsFromDi()),] })
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoadingSpinnerComponent,
+    RegComponent,
+    LoginComponent,
+    HomeComponent,
+    AccountComponent,
+    AdminComponent,
+    MenuComponent,
+    ThemSanPhamComponent,
+    TabNavigationComponent,
+    SanphamComponent,
+    LienheComponent,
+    ChinhsachComponent,
+    RankComponent,
+    ThongtinsanphamComponent,
+    GioHangComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgChartsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    SlickCarouselModule 
+  ],
+  providers: [
+    provideHttpClient(withFetch()),
+    provideHttpClient(withInterceptorsFromDi()),
+  ],
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA] 
+})
 export class AppModule { }
--- src/app/page/home/home.component.ts
+++ src/app/page/home/home.component.ts
@@ -8,6 +8,7 @@ import { Router } from '@angular/router';
     standalone: false
 })
 export class HomeComponent {
+  
 
   constructor(private router: Router) { }
 
@@ -30,19 +31,42 @@ export class HomeComponent {
     this.router.navigate(['/account']);
   }
  
-    imageList = [
-      'assets\anh\bong1.png',
-      'assets\anh\bong2.png',
-      'assets\anh\bong3.png',
-    ];
+  imageList = [
+    'assets/anh/anh-bong-da-ronaldo.jpg',
+    'assets/anh/hinh-nen-bong-da-2.jpg',
+    'assets/anh/Anh-qua-bong-da.jpg',
+    
+  ];
   
-    slideConfig = {
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      autoplay: true,
-      autoplaySpeed: 3000,
-      dots: true,
-      arrows: false
-    };
+  slideConfig = {
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    dots: true,
+    arrows: false
+  };
+  infoList = [
+    {
+      icon: 'https://cdn-icons-png.flaticon.com/512/5956/5956476.png',
+      title: 'KHÔNG SỢ HẾT SIZE',
+      description: 'Do chẳng cần đợi nhân viên chốt đơn'
+    },
+    {
+      icon: 'https://cdn-icons-png.flaticon.com/512/1548/1548682.png',
+      title: 'GIAO HÀNG TOÀN QUỐC',
+      description: 'Gửi hàng đi luôn trong ngày'
+    },
+    {
+      icon: 'https://cdn-icons-png.flaticon.com/512/4290/4290854.png',
+      title: 'THANH TOÁN LINH HOẠT',
+      description: 'Tiền mặt/CK/ví điện tử/thẻ'
+    },
+    {
+      icon: 'https://cdn-icons-png.flaticon.com/512/4221/4221442.png',
+      title: 'ĐỔI SIZE THOẢI MÁI',
+      description: 'Đến khi anh em hài lòng'
+    }
+  ];
   }
 
